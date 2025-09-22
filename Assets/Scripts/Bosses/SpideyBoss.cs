35fff4bdc5b90b54d9b24749afa4a7bda8973346
Inka <inka@Inkas-MacBook-Pro.local>
2025-09-22 13:50:19 +0300

    added spidey better

---
+++ Assets/Scripts/Bosses/SpideyBoss.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class SpideyBoss : MonoBehaviour
+{
+//	What do we need?
+
+/*
+	The legs
+		- calculate the radius for movement leg_nmb (starting point for the round) + movement_radius (pi * ?)
+		- how much do we want to move from the joint? 
+
+	void LegMoveLoop()
+	{
+		Choose random leg
+			- Move up, turn(random nmb in radius), move down
+		Wait (random time in radius)
+	}
+	
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+	The eyes follow Momma
+*/
+	private int leg_num;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+/*
+	void LegMoveLoop()
+	{
+		Choose random leg
+			- Move up, turn(random nmb in radius), move down
+		Wait (random time in radius)
+	}
+	*/
+}
