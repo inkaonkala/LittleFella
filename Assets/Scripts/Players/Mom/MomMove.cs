f53a6dcdc88939047e3c39353966e76988af973a
inkaonkala <inkaisinka@live.com>
2025-09-25 13:59:13 +0300

    hit added

--- Assets/Scripts/Players/Mom/MomMove.cs
+++ Assets/Scripts/Players/Mom/MomMove.cs
@@ -4,13 +4,13 @@ using UnityEngine.InputSystem;
 public class MomMove : MonoBehaviour
 {
     public float moveSpeed = 5f;
-
     public float jumpForce = 20f;
     public int jumpMax = 2;
 
     public Transform groundCheck;
-    public LayerMask groundLayer; private Rigidbody2D body;
+    public LayerMask groundLayer;
 
+    private Rigidbody2D body;
     private Vector2 movement;
     private MomInput input;
 
@@ -20,6 +20,10 @@ public class MomMove : MonoBehaviour
     private Animator animatoor;
     private SpriteRenderer sr;
 
+    private void Awake()
+    {
+        input = new MomInput();
+    }
 
     void Start()
     {
@@ -29,30 +33,41 @@ public class MomMove : MonoBehaviour
         sr = GetComponentInChildren<SpriteRenderer>();
     }
 
-    private void Awake()
-    {
-        input = new MomInput();
-    }
-
     private void OnEnable()
     {
         input.Mom.Enable();
-        input.Mom.Move.performed += ctx => movement = ctx.ReadValue<Vector2>();
-        input.Mom.Move.canceled += ctx => movement = Vector2.zero;
-        input.Mom.Jump.performed += ctx => TryJump();
+
+        /*
+                input.Mom.Move.performed += ctx => movement = ctx.ReadValue<Vector2>();
+                input.Mom.Move.canceled += ctx => movement = Vector2.zero;
+                input.Mom.Jump.performed += ctx => TryJump();
+                input.Mom.Smack.performed += OnSmack;
+                */
+                input.Mom.Move.performed += OnMove;
+        input.Mom.Move.canceled  += OnMoveCanceled;
+        input.Mom.Jump.performed += OnJump;
+        input.Mom.Smack.performed += OnSmack;   // <-- this matches the delegate        input.Mom.Move.performed += OnMove;
+        input.Mom.Move.canceled  += OnMoveCanceled;
+        input.Mom.Jump.performed += OnJump;
     }
 
     private void OnDisable()
     {
+        input.Mom.Move.performed -= OnMove;
+        input.Mom.Move.canceled  -= OnMoveCanceled;
+        input.Mom.Jump.performed -= OnJump;
+        input.Mom.Smack.performed -= OnSmack;
+
         input.Mom.Disable();
     }
 
     void Update()
-   {
+    {
         animatoor.SetBool("isWalking", Mathf.Abs(movement.x) > 0.01f);
 
         if (movement.x != 0f)
             sr.flipX = movement.x > 0f;   // if this is reversed, change to `> 0f`
+            
     }
 
     void FixedUpdate()
@@ -67,7 +82,29 @@ public class MomMove : MonoBehaviour
     //    Debug.Log("Is Grounded: " + isGrounded);
         //body.MovePosition(body.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
-    
+
+
+    // ===== Handlers (correct signatures) =====
+    private void OnMove(InputAction.CallbackContext ctx)
+    {
+        movement = ctx.ReadValue<Vector2>();
+    }
+
+    private void OnMoveCanceled(InputAction.CallbackContext ctx)
+    {
+        movement = Vector2.zero;
+    }
+
+    private void OnJump(InputAction.CallbackContext ctx)
+    {
+        TryJump();
+    }
+
+    private void OnSmack(InputAction.CallbackContext ctx)
+    {
+        animatoor.SetTrigger("Hit");
+    }
+    // ========================================
     private void TryJump()
     {
         if (isGrounded || jumpsLeft > 0)
